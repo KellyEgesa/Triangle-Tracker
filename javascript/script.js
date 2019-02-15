5b8860c063809e6cbbc63c76a28987bd1bb78e00
KellyEgesa <kelly.egesa@gmail.com>
2019-02-15 14:27:40 +0300

    linked html and javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,26 @@
-//Business Logic
-var a = prompt("Input side a:");
-var b = prompt("Input side b:");
-var c = prompt("Input side c:");
-if(a+b<=c || b+a<=c || c+b<=a){
-	alert("Not a Triangle")
-}
-else{
-	if (a == c && a==b && b==c){
-		alert("Equilateral Triangle");
-	}
-	else if(a == c || a==b || b==c){
-		alert("Isosceles Triangle");
-	}
-	else{
-		if( a+b>c || b+c>a || c+b>a){
-  		alert("Scalene Triangle")
-  	}
-  	else{
- 		 alert("Not a Triangle")
-  	}
-	}
- }
+
+function page () {
+  var Tri = [];
+  var a = parseInt(Tri.push(document.getElementById('a').value));
+  var b = parseInt(Tri.push(document.getElementById('b').value));
+  var c = parseInt(Tri.push(document.getElementById('c').value));
+  //Business Logic
+  var Triangle = ["Equilateral Triangle","Isosceles Triangle","Scalene Triangle","Not a Triangle"];
+  if((a+b)<=c || (b+c)<=a || (c+a)<=b){
+    document.getElementById("output").innerHTML=Triangle[3];
+  }
+  else
+    if (a == c && a==b && b==c){
+      document.getElementById("output").innerHTML=Triangle[0];
+    }
+    else if(a == c || a==b || b==c){
+      document.getElementById("output").innerHTML=Triangle[1];
+    }
+    else
+      if( a+b>c || b+c>a || c+b>a){
+        document.getElementById("output").innerHTML=Triangle[2];
+      }
+      else{
+        document.getElementById("output").innerHTML=Triangle[3];
+      }
+  }
